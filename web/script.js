1895d8f8fa9a6302b146b67a58c018aca11a6db6
Silvio Almeida <silviotmalmeida@gmail.com>
2020-05-30 18:40:31 -0300

    Criando cobrinha

--- web/script.js
+++ web/script.js
@@ -1,6 +1,10 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [{    
+    x: 8 * box,
+    y: 8 * box
+}];
 
 function criarBG(){
 
@@ -8,4 +12,14 @@ function criarBG(){
     context.fillRect(0, 0, 16* box, 16 * box);
 }
 
+function criarCobrinha(){
+
+    for(let i=0; i < snake.length; i++){
+
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
 criarBG();
+criarCobrinha();

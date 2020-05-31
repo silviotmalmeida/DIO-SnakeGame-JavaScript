339a17da08985e28785cb85713724301d8569481
Silvio Almeida <silviotmalmeida@gmail.com>
2020-05-31 13:02:59 -0300

    Improving the moviment oh the snake

--- web/script.js
+++ web/script.js
@@ -23,6 +23,27 @@ function createSnake(){
     }
 }
 
+function update(event){
+
+    //the keycode 39 represents the arrowright key
+    //the new direction of the snake's head cannot be the reverse of the previously direction
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+
+    //the keycode 37 represents the arrowleft key
+    //the new direction of the snake's head cannot be the reverse of the previously direction
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+
+    //the keycode 40 represents the arrowdown key
+    //the new direction of the snake's head cannot be the reverse of the previously direction
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+
+    //the keycode 38 represents the arrowup key
+    //the new direction of the snake's head cannot be the reverse of the previously direction
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+}
+//Includind the event listener for the keyboard commands
+document.addEventListener('keydown', update);
+
 function startGame(){
 
     //creating the background of the canvas
@@ -35,16 +56,23 @@ function startGame(){
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
+    //getting the new position of the snake's head
     if(direction == "right") snakeX += box;
     if(direction == "left") snakeX -= box;
     if(direction == "down") snakeY += box;
     if(direction == "up") snakeY -= box;
 
+    //adjusting the position of the snake's head avoiding the border of the canvas 
+    if(snakeX > 15 * box && direction == "right") snakeX = 0;
+    if(snakeX < 0 && direction == "left") snakeX = 15 * box;
+    if(snakeY > 15 * box && direction == "down") snakeY = 0;
+    if(snakeY < 0 && direction == "up") snakeY = 15 * box;
+
     //the method pop() removes the last item of an array
     //erasing the snake's head   
     snake.pop();
     
-    //getting the new position of the snake's head
+    //setting the new position of the snake's head
     let newHead = {
         x: snakeX,
         y: snakeY
